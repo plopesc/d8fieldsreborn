cdf07987a12872152bdc6a8d3d98352f85ad1858
Pablo López <plopesc@gmail.com>
2014-05-16 18:40:03 +0200

    patch

--- source/FormatterInterface.php
+++ source/FormatterInterface.php
@@ -1,6 +1,7 @@
 namespace Drupal\Core\Field;
 
 interface FormatterInterface extends PluginSettingsInterface {
+  public static function defaultSettings()
   public function settingsForm(array $form, array &$form_state);
   public function settingsSummary();
   public function prepareView(array $entities_items);
--- source/WidgetInterface.php
+++ source/WidgetInterface.php
@@ -1,4 +1,5 @@
 interface WidgetInterface extends WidgetBaseInterface {
+  public static function defaultSettings()
   public function settingsForm(array $form, array &$form_state);
   public function settingsSummary();
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, array &$form_state);
